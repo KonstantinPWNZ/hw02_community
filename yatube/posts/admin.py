4e9da2c898ee849f2d5e504e427f6e4cd5ebbf2b
Konstantin SM <s1662w@gmail.com>
2021-07-12 02:38:56 +0300

    Total

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -13,11 +13,8 @@ admin.site.register(Group, GroupAdmin)
 
 
 class PostAdmin(admin.ModelAdmin):
-    # перечисляем поля, которые должны отображаться в админке
     list_display = ("pk", "text", "pub_date", "author")
-    # добавляем интерфейс для поиска по тексту постов
     search_fields = ("text",)
-    # добавляем возможность фильтрации по дате
     list_filter = ("pub_date",)
     empty_value_display = "-пусто-"  # где пусто - там будет эта строка
 
